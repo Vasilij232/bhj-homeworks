025e39fbdacbf0a3b9e80f8d5a7968ec3f8d2546
Vasili Fitkalenko <astapovvasilij@gmail.com>
2026-03-10 23:10:52 +0400

    first commit progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	const form = document.getElementById('form');
 	const progress = document.getElementById('progress');
 	const sendBtn = document.getElementById('send');
